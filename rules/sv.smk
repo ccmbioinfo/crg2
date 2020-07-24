e5a7e4cbf439408e833f5788355a4fa684f1fe6f
dennis-kao <d.kao@live.com>
2020-07-24 12:26:22 -0400

    Added a PASS rule to SV. Made PASS rule more generic. It can now match to any vcf and vcf.gz file.

--- rules/sv.smk
+++ rules/sv.smk
@@ -85,7 +85,7 @@ rule metasv:
 
 rule snpeff:
     input:
-        "sv/metasv/{sample}-{unit}/variants.vcf.gz"
+        "sv/metasv/{sample}-{unit}/variants.pass.vcf.gz"
     output:
         temp("sv/metasv/{sample}-{unit}/variants.snpeff.vcf")
     log:
--- wrappers/bcftools/view/wrapper.py
+++ wrappers/bcftools/view/wrapper.py
@@ -6,7 +6,19 @@ __license__ = "MIT"
 
 from snakemake.shell import shell
 
+outfile = snakemake.output[0]
+
+compress_flags = ""
+
+if outfile.endswith("vcf.gz"):
+    compress_flags = "-Oz"
+elif outfile.endswith("vcf"):
+    compress_flags = "-Ov"
+elif outfile.endswith("bcf"):
+    compress_flags = "-Ou"
+elif outfile.endswith("bcf.gz"):
+    compress_flags = "-Ob"
 
 shell(
-    "bcftools view {snakemake.params} {snakemake.input[0]} " "-o {snakemake.output[0]}"
+    "bcftools view {snakemake.params} {snakemake.input[0]} {compress_flags} " "-o {outfile}"
 )
