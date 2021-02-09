3afc009ac7f743a47741845f432832d7712a013f
Aarthi <8120708+aarthi-mohan@users.noreply.github.com>
2021-02-09 11:18:12 -0500

    added threads to bcftools concat

--- wrappers/bcftools/concat/wrapper.py
+++ wrappers/bcftools/concat/wrapper.py
@@ -6,8 +6,12 @@ __license__ = "MIT"
 
 from snakemake.shell import shell
 
+if not {snakemake.threads}:
+    threads = 8
+else:
+    threads =  {snakemake.threads}
 
 shell(
-    "bcftools concat {snakemake.params} -o {snakemake.output[0]} "
+    "bcftools concat {snakemake.params} --threads {threads} -o {snakemake.output[0]} "
     "{snakemake.input.calls}"
 )
