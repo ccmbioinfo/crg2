095486f12768cfdfb21966174bf97e5d3df0b76e
aarthi-mohan <aarthimohan11@gmail.com>
2021-02-24 14:59:49 -0500

    add AC and decompose platypus variants using vcflib

--- wrappers/platypus/wrapper.py
+++ wrappers/platypus/wrapper.py
@@ -22,12 +22,15 @@ if isinstance(bams, list):
     bams = ",".join(bams)
 bams = "--bamFiles={}".format(bams)
 
-params = {snakemake.params} if {snakemake.params} else ""
+params = snakemake.params if snakemake.params else ""
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 shell(
     "platypus callVariants {bams} {params} --nCPU {threads} "
     "--refFile={snakemake.input.ref} {regions} "
-    "--output={snakemake.output} {log}"
+    "--output={snakemake.output}  &&  "
+    "vcfallelicprimitives -t DECOMPOSED --keep-geno {snakemake.output} "
+    "| vcffixup - | vcfstreamsort  > temp && "
+    "mv temp {snakemake.output} {log} "
 )
