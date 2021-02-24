c74ee979740beb84ce1b3581ae49a7131e8eb46b
aarthi-mohan <aarthimohan11@gmail.com>
2021-02-24 15:06:18 -0500

    added output format options

--- wrappers/bcftools/concat/wrapper.py
+++ wrappers/bcftools/concat/wrapper.py
@@ -8,12 +8,25 @@ from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-if not {snakemake.threads}:
+outfile = snakemake.output[0]
+
+compress_flags = ""
+
+if outfile.endswith("vcf.gz"):
+    compress_flags = "-Oz"
+elif outfile.endswith("vcf"):
+    compress_flags = "-Ov"
+elif outfile.endswith("bcf"):
+    compress_flags = "-Ou"
+elif outfile.endswith("bcf.gz"):
+    compress_flags = "-Ob"
+
+if not snakemake.threads:
     threads = 8
 else:
-    threads =  {snakemake.threads}
+    threads =  snakemake.threads
 
 shell(
-    "bcftools concat {snakemake.params} --threads {threads} -o {snakemake.output[0]} "
+    "bcftools concat {snakemake.params} --threads {threads} {compress_flags} -o {snakemake.output[0]} "
     "{snakemake.input.vcf} {log}"
 )
