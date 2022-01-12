5ef2190512582aaf6db877edb7ace0f9270a7b19
NourHanafi <nour.hanafi@mail.utoronto.ca>
2022-01-12 13:23:05 -0500

    added rm command to wrapper to remove cram and accessory files created

--- wrappers/samtools/fastq/wrapper.py
+++ wrappers/samtools/fastq/wrapper.py
@@ -24,4 +24,6 @@ reheader_cmd = (" samtools reheader -i fastq/{family}_{sample}_header.sam {dest}
 
 fastq_cmd = (" samtools sort -n {dest} | samtools fastq - --reference {snakemake.params.NewCramRef} -1 fastq/{family}_{sample}_R1.fastq.gz -2 fastq/{family}_{sample}_R2.fastq.gz -s fastq/{family}_{sample}_singleton.fastq.gz; ")
 
-shell("(" + copy_cmd + get_header_cmd + sed_cmd + reheader_cmd + fastq_cmd + ") {log}")
+rm_cmd = (" rm -f fastq/{family}_{sample}_header.sam fastq/{family}_{sample}.cram fastq/{family}_{sample}_singleton.fastq.gz; ")
+
+shell("(" + copy_cmd + get_header_cmd + sed_cmd + reheader_cmd + fastq_cmd + rm_cmd + ") {log}")
