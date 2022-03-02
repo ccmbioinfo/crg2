208e7df30183642b6a1a40c24b6d80769b524ee0
pamelaxu213 <pamela.xu@mail.utoronto.ca>
2022-03-02 10:12:36 -0500

    modified scripts

--- wrappers/bcftools/merge/wrapper.py
+++ wrappers/bcftools/merge/wrapper.py
@@ -13,13 +13,19 @@ log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 input = " ".join(snakemake.input.vcf)
 
 # if one sample, pass this rule, else merge into multisample vcf
-if len(snakemake.input) == 1:
+if len(snakemake.input.vcf) == 1:
     shell(
-    "mv {snakemake.input[0]} {snakemake.output[0]} "
+    "mv {snakemake.input.vcf} {snakemake.output.vcf_unsort} "
     )
+
 else:
     shell(
-    "bcftools merge {extra} -o {snakemake.output[0]} "
-    "{input} | bcftools sort {snakemake.output[0]} "
-    "-o {snakemake.output.vcf} "
+    "bcftools merge {extra} -o {snakemake.output.vcf_unsort} "
+    "{input} "
     )
+
+
+shell(
+"bcftools sort {snakemake.output.vcf_unsort} "
+"-o {snakemake.output.vcf} "
+)
