9e06285b07c604af89924b40d1f44709407e1e75
Madelinehazel <mhcouse@gmail.com>
2022-04-08 10:11:27 -0400

    add command-line arguments

--- dnaseq_slurm_api.sh
+++ dnaseq_slurm_api.sh
@@ -1,37 +1,41 @@
-# sample commands to be called by stager slurm.py with variable substitution
+#!/usr/bin/env bash
 # assumes crg2 is installed in home directory of user
 # in production, crg2 is located in /home/ccmmarvin
-# {family}, {analysis_id}, {participant}, {linked_files} are passed to the slurm API by slurm.py
+# ANALYSIS_ID, FAMILY, and DATA are provided by STAGER
+
+set -euo pipefail
 
 SF=~/crg2/Snakefile; 
 CP="/home/slurm/conda_envs/crg2-conda";
 SLURM=~/crg2/slurm_profile;
+ANALYSIS_ID="$1"
+FAMILY="$2"
+DATA="$3"
+#temporary filepath
+FILEPATH=/storage/data/test_crg2_automation
+ANALYSIS_DIR=${FILEPATH}/${ANALYSIS_ID}/${FAMILY}
 
 source /storage/modules/anaconda/2020.11/etc/profile.d/conda.sh
 conda activate snakemake
 
-#temporary filepath
-filepath=/storage/data/test_crg2_automation
-analysis_dir=${filepath}/{analysis_id}/{family}
-
-if [ ! -d $analysis_dir ];then
-    mkdir  -p $analysis_dir
+if [ ! -d $ANALYSIS_DIR ];then
+    mkdir  -p $ANALYSIS_DIR
 else
-    echo "Analysis directory $analysis_dir exists, exiting"
+    echo "Analysis directory $ANALYSIS_DIR exists, exiting"
     exit 1
 fi
 
-cd $analysis_dir
+cd $ANALYSIS_DIR
 
 python3 exome_setup.py \
-    -a analysis_dir \
-    -f {family} \
-    -d {participant}:{linked_files} {participant}:{linked_files} {participant}:{linked_files}
+    -a $ANALYSIS_DIR \
+    -f $FAMILY \
+    -d $DATA
 
 exit_code=`echo $?`
 if [ $exit_code == 0 ]; then
     snakemake --use-conda -s $SF --conda-prefix $CP  --profile $SLURM -p 
 else
     echo 'Analysis setup failed, exiting'
-    exit
+    exit 1
 fi
