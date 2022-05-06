98456c21cde866edbead8a06e5740f1ca892fbd9
pamelaxu213 <pamela.xu@mail.utoronto.ca>
2022-05-06 14:20:53 -0400

    add wrapper for rtg vcfsubset

---
+++ wrappers/rtg-tools/vcfsubset/wrapper.py
@@ -0,0 +1,15 @@
+from snakemake.shell import shell
+
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+java_opts = snakemake.params.get("java_opts")
+
+
+shell(
+    "export _JAVA_OPTIONS=\"{java_opts}\" && "
+    "rtg vcfsubset -i {snakemake.input} -o annotated/mosaic/vcfanno/noDP.vcf.gz --remove-info DP && "
+    "bcftools +fill-tags annotated/mosaic/vcfanno/noDP.vcf.gz -o {snakemake.output} -- -t 'DP2=sum(DP)' && "
+    "sed -i \"s/DP2/DP/g\" {snakemake.output} && "
+    "rm annotated/mosaic/vcfanno/noDP.vcf* "
+
+)
