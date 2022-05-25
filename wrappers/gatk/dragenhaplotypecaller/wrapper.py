52a874e35eec5852d7be6e208341d355fa9aab51
NourHanafi <nour.hanafi@mail.utoronto.ca>
2022-05-25 16:27:35 -0400

    added dragen-gatk as an option to run vs. gatk or gatk3

---
+++ wrappers/gatk/dragenhaplotypecaller/wrapper.py
@@ -0,0 +1,34 @@
+__author__ = "Johannes Köster"
+__copyright__ = "Copyright 2018, Johannes Köster"
+__email__ = "johannes.koester@protonmail.com"
+__license__ = "MIT"
+
+
+import os
+
+from snakemake.shell import shell
+
+known = snakemake.input.get("known", "")
+if known:
+    known = "--dbsnp " + known
+
+extra = snakemake.params.get("extra", "")
+java_opts = snakemake.params.get("java_opts", "")
+bams = snakemake.input.bam
+regions = snakemake.input.get("regions", "")
+if regions:
+    extra = extra + " -L {}".format(regions)
+
+if isinstance(bams, str):
+    bams = [bams]
+bams = list(map("-I {}".format, bams))
+
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+shell(
+    "gatk --java-options '{java_opts}' HaplotypeCaller {extra} "
+    "-R {snakemake.input.ref} {bams} "
+    "-ERC GVCF "
+    "--dragen-mode "
+    "--dragstr-params-path {snakemake.input.dragstr_parameters} "
+    "-O {snakemake.output.gvcf} {known} {log}"
+)
