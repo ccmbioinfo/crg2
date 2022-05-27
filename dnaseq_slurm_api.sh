31bbeb6e62981a48d0f1f130268a3ab12dfb5c18
Madelinehazel <mhcouse@gmail.com>
2022-05-27 12:56:02 -0400

    cleanup script

--- dnaseq_slurm_api.sh
+++ dnaseq_slurm_api.sh
@@ -1,25 +1,20 @@
 #!/usr/bin/env bash
-# assumes crg2 is installed in home directory of user
-# in production, crg2 is located in /home/ccmmarvin
 # ANALYSIS_ID, FAMILY, and DATA are provided by STAGER
 
 set -euo pipefail
 
 # set environment variables
-export ANNOTSV=/srv/shared/data/AnnotSV_2.1
-#export LC_ALL=C.UTF-8
-#export LANG=C.UTF-8
+export ANNOTSV=/srv/shared/data/dccdipg/crg2-non-conda-tools/AnnotSV_2.1
 
 # set pipeline variables
-SF=/srv/shared/pipelines/crg2/Snakefile; 
+SF="/srv/shared/pipelines/crg2/Snakefile"; 
 CP="/srv/shared/conda_envs/crg2-conda/";
-SLURM=/srv/shared/pipelines/crg2/slurm_profile;
+SLURM="/srv/shared/pipelines/crg2/slurm_profile";
 ANALYSIS_ID="$1"
 FAMILY="$2"
 # DATA is a json string in the format { participant1: [linked files], participant2: ['linked files']...}
 DATA="$3"
-#temporary filepath
-FILEPATH=/srv/shared/analyses/exomes
+FILEPATH="/srv/shared/analyses/exomes"
 ANALYSIS_DIR="${FILEPATH}/${ANALYSIS_ID}/${FAMILY}"
 
 source /storage/modules/anaconda/2020.11/etc/profile.d/conda.sh
