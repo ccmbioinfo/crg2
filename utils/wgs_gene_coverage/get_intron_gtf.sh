4929be9053ab8427850d1915c2702e99b8b8414b
ShaniaWu <shania.wu@login2.ccm.sickkids.ca>
2023-07-10 16:26:04 -0400

    update parse_gtf.R for hpf paths + run get_intron_gtf.sh shell commands from R

--- utils/wgs_gene_coverage/get_intron_gtf.sh
+++ utils/wgs_gene_coverage/get_intron_gtf.sh
@@ -1,2 +1,3 @@
+DATA_DIR=~/crg2/utils/wgs_gene_coverage/data;
 
-bedtools subtract -a ./*.transcripts.gtf -b ./*.exons.gtf > hg19.ncbiRefSeq.introns.gtf
+bedtools subtract -a ${DATA_DIR}/hg19.ncbiRefSeq.transcripts.gtf -b ${DATA_DIR}/hg19.ncbiRefSeq.exons.gtf > ${DATA_DIR}/hg19.ncbiRefSeq.introns.gtf
--- utils/wgs_gene_coverage/parse_gtf.R
+++ utils/wgs_gene_coverage/parse_gtf.R
@@ -1,49 +1,60 @@
 library(data.table)
 library(rtracklayer)
 
+dir <- "~/crg2/utils/wgs_gene_coverage/"
 
-gtf <- readGFF("/Users/shaniawu/Desktop/Care4Rare/data/hg19.ncbiRefSeq.gtf")
+
+print("Reading GFT file...")
+gtf <- readGFF("/hpf/largeprojects/ccm_dccforge/dccdipg/Common/genomes/hg19.ncbiRefSeq.gtf")
 
 ## Filter out additional chromosomes 
 chr_names <- paste("chr", c(seq(1:22), "X", "Y"), sep="")
 gtf <- gtf[which(gtf$seqid %in% chr_names),]
 
+## Make "data" folder
+dir.create(paste(dir, "data", sep=""))
 
-## Export transcripts only
+
+## Export transcripts only ---
+print("Getting transcripts...")
 transcripts <- gtf[which(gtf$type == "transcript"),]
-export(transcripts, "/Users/shaniawu/Desktop/Care4Rare/data/hg19.ncbiRefSeq.transcripts.gtf")
+export(transcripts, paste(dir, "data/hg19.ncbiRefSeq.transcripts.gtf", sep="")) # creates
 
-## Export exons only
+## Export exons only ---
+print("Getting exons...")
 exons <- gtf[which(gtf$type == "exon"),]
-export(exons, "/Users/shaniawu/Desktop/Care4Rare/data/hg19.ncbiRefSeq.exons.gtf")
+export(exons, paste(dir, "data/hg19.ncbiRefSeq.exons.gtf", sep=""))
 exons$ID <- NA
 
 
-##### After running get_intron_gft.sh ---
-## (uses "bedtools subtract" (transcript-exon) to extract intron coordinates)
-introns <- readGFF("/Users/shaniawu/Desktop/Care4Rare/data/hg19.ncbiRefSeq.introns.gtf")
+## Get intron coordinates using bedtools subtract (transcript-exon) ---
+print("Getting introns...")
+system("DATA_DIR=~/crg2/utils/wgs_gene_coverage/data; bedtools subtract -a ${DATA_DIR}/hg19.ncbiRefSeq.transcripts.gtf -b ${DATA_DIR}/hg19.ncbiRefSeq.exons.gtf > ${DATA_DIR}/hg19.ncbiRefSeq.introns.gtf")
+
+introns <- readGFF(paste(dir, "data/hg19.ncbiRefSeq.introns.gtf", sep=""))
 introns$type <- "intron" 
 introns$exon_id <- NA
 introns$exon_number <- NA
-export(introns, "/Users/shaniawu/Desktop/Care4Rare/data/hg19.ncbiRefSeq.introns.gtf")
 
 
-## Merge exons & introns into one BED file
+## Merge exons & introns into one BED file & format ---
+print("Formatting and creating exons+introns BED...")
 exons_introns_gtf <- rbind(exons, introns)
-export(exons_introns_gtf, "/Users/shaniawu/Desktop/Care4Rare/data/hg19.ncbiRefSeq.exon+intron.gtf")
-# exons_introns_gtf <- readGFF("/Users/shaniawu/Desktop/Care4Rare/data/hg19.ncbiRefSeq.exon+intron.gtf")
-
-
-# Remove "chr" prefix 
-exons_introns_gtf$seqid <- gsub("chr", "", exons_introns_gtf$seqid) 
-
-
-## Extract columns from GTF for BED file ---
-# Note: GTF file provides exon transcript and number for exons ("exon_id"), 
-#       but only transcript id for introns
+exons_introns_gtf$seqid <- gsub("chr", "", exons_introns_gtf$seqid) # Remove "chr" prefix 
 
 bed <- subset(exons_introns_gtf, select = c("seqid", "start", "end", "type", "gene_id", "transcript_id"))
 
-write.table(bed, "/Users/shaniawu/Desktop/Care4Rare/data/hg19_introns_exons.bed", row.names = F)
-# also saved to /hpf/largeprojects/ccm_dccforge/dccdipg/Common/genomes/hg19_introns_exons.bed
 
+## Write BED file ---
+write.table(bed, (paste(dir, "data/hg19_introns_exons.bed", sep="")), row.names = F)
+# copy of file in /hpf/largeprojects/ccm_dccforge/dccdipg/Common/genomes/hg19_introns_exons.bed used in get_gene_exon_intron.R
+
+print("Done. BED file saved to hg19_introns_exons.bed.")
+
+
+## Delete interediate files
+del <- c(paste(dir, "data/hg19.ncbiRefSeq.transcripts.gtf", sep=""),
+        paste(dir, "data/hg19.ncbiRefSeq.exons.gtf", sep=""),
+        paste(dir, "data/hg19.ncbiRefSeq.introns.gtf", sep=""))
+unlink(del)
+print("Deleted intermediate files.")
