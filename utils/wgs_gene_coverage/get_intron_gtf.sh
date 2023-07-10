830259f1a7d589b05946551c229dfb71d6529f6f
ShaniaWu <shania.wu@login2.ccm.sickkids.ca>
2023-07-10 16:28:04 -0400

    delete get_intron_gtf.sh (commands replaced in parse_gtf.R)

--- utils/wgs_gene_coverage/get_intron_gtf.sh
+++
@@ -1,3 +0,0 @@
-DATA_DIR=~/crg2/utils/wgs_gene_coverage/data;
-
-bedtools subtract -a ${DATA_DIR}/hg19.ncbiRefSeq.transcripts.gtf -b ${DATA_DIR}/hg19.ncbiRefSeq.exons.gtf > ${DATA_DIR}/hg19.ncbiRefSeq.introns.gtf
