ee20081d0057a51168d8df2e255443bb6762b22e
Madelinehazel <mhcouse@gmail.com>
2025-01-03 19:06:22 +0000

    code dependencies in env yaml for mity

--- wrappers/mity/report/wrapper.py
+++ wrappers/mity/report/wrapper.py
@@ -10,13 +10,11 @@ prefix = snakemake.params.prefix
 tool = snakemake.params.tool
 pythonpath = tool.replace("bin", "")
 
-mod1 = " module load python/3.9.2_torch_gpu; "
-mod2 = " module load freebayes/1.3.1; "
 python = " export PYTHONPATH={pythonpath}; "
 bgzip = " bgzip {snakemake.input}; "
 mity = " {tool}/mity report --prefix {prefix} --out-folder-path {outdir} {snakemake.input}.gz; "
 remove_excel = " rm report/mitochondrial/{family}_mito.annotated_variants.xlsx; "
-shell("(" + mod1 + mod2 + python + bgzip + mity + remove_excel + ") {log}")
+shell("(" + python + bgzip + mity + remove_excel + ") {log}")
 
 report = f"report/mitochondrial/{family}_mito.annotated_variants.csv"
 process_report.main(f"{snakemake.input}.gz", report, family)
