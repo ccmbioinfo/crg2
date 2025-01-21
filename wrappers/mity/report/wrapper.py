60519f85294693b29206ad1703660b55c3950d2e
Anjali Jain <anjali.jain2299@gmail.com>
2025-01-21 09:41:34 -0500

    Load python and freebayes module for HPF

--- wrappers/mity/report/wrapper.py
+++ wrappers/mity/report/wrapper.py
@@ -2,7 +2,7 @@ from snakemake.shell import shell
 import os
 import process_report
 
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+log = snakemake.log_fmt_shell(stdout=True, stderr=True, append=True)
 
 family = snakemake.wildcards.family
 outdir = snakemake.params.outdir
@@ -10,11 +10,17 @@ prefix = snakemake.params.prefix
 tool = snakemake.params.tool
 pythonpath = tool.replace("bin", "")
 
+mod1 = " module load python/3.9.2_torch_gpu; "
+mod2 = " module load freebayes/1.3.1; "
 python = " export PYTHONPATH={pythonpath}; "
 bgzip = " bgzip {snakemake.input}; "
 mity = " {tool}/mity report --prefix {prefix} --out-folder-path {outdir} {snakemake.input}.gz; "
 remove_excel = " rm report/mitochondrial/{family}_mito.annotated_variants.xlsx; "
-shell("(" + python + bgzip + mity + remove_excel + ") {log}")
+
+try: # hpf
+    shell("(" + mod1 + mod2 + python + mity + remove_excel + ") {log}")
+except:
+    shell("(" + python + bgzip + mity + remove_excel + ") {log}")
 
 report = f"report/mitochondrial/{family}_mito.annotated_variants.csv"
 process_report.main(f"{snakemake.input}.gz", report, family)
