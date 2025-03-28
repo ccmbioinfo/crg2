29c19b0e2adb902a269de3e2e46f4772801a2d3a
Anjali Jain <anjali.jain2299@gmail.com>
2025-03-28 11:51:12 -0400

    Temporary fix to prevent xlsxwriter exception wrt sheetname length

--- wrappers/mity/report/wrapper.py
+++ wrappers/mity/report/wrapper.py
@@ -8,6 +8,23 @@ tool = snakemake.params.tool
 pythonpath = tool.replace("bin", "")
 
 python = " export PYTHONPATH={pythonpath}; "
-mity = " {tool}/mity report --prefix {family} -k --output-dir {outdir} {snakemake.input};"
-rename_file= " mv mitochondrial_variants/{family}.mity.normalise.decompose.mity.annotated.vcf mitochondrial_variants/{family}.mity.annotated.vcf"
-shell("(" + python + mity + rename_file + ") {log}")
+
+# Temporary fix until mity fixes bug in their code base
+# Creating a copy of the input file with a different name to prevent xlsxwriter from throwing 
+# "xlsxwriter.exceptions.InvalidWorksheetName: Excel worksheet name '...' must be <= 31 chars." error
+
+orig_input_filename=f"{snakemake.input}"
+new_filename=f"mitochondrial_variants/{family}.vcf.gz"
+copy_input_file = " cp {orig_input_filename} {new_filename}; "
+copy_input_file_tbi = " cp {orig_input_filename}.tbi {new_filename}.tbi; "
+
+shell ("(" + copy_input_file + copy_input_file_tbi + ") {log} ")
+
+mity = " {tool}/mity report --prefix {family} -k --output-dir {outdir} {new_filename};"
+#rename_vcf_file= " mv mitochondrial_variants/{family}.mity.normalise.decompose.mity.annotated.vcf mitochondrial_variants/{family}.mity.annotated.vcf ; "
+
+# Remove input file copy
+rm_copy= "rm {new_filename}; "
+rm_copy_tbi= "rm {new_filename}.tbi;"
+
+shell("(" + python + mity + rm_copy + rm_copy_tbi +") {log}")
