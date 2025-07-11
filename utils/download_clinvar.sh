160ce83b2be68d8a519640490492a2d8ffc1f888
Madeline Couse <mhcouse@gmail.com>
2025-07-11 10:24:47 -0400

    fix output directory and md5sum check

--- utils/download_clinvar.sh
+++ utils/download_clinvar.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # to run this script automatically on a monthly basis, add the following line to your crontab using `crontab -e`:
 # 0 0 15 * * sh ~/crg2/utils/download_clinvar.sh
+set -e
 DATE=`date +%Y-%m-%d`
 DIR_HG19=/hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37/variation/
 DIR_HG38=/hpf/largeprojects/ccmbio/nhanafi/c4r/downloads/databases/
@@ -9,17 +10,21 @@ DIR_HG38=/hpf/largeprojects/ccmbio/nhanafi/c4r/downloads/databases/
 mv ${DIR_HG19}/clinvar.vcf.gz ${DIR_HG19}/clinvar_old/clinvar.${DATE}.vcf.gz
 rm ${DIR_HG19}/clinvar.vcf.gz.tbi ${DIR_HG19}/clinvar.vcf.gz.md5
 
-wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz > ${DIR_HG19}/clinvar.vcf.gz
-wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz.tbi > ${DIR_HG19}/clinvar.vcf.gz.tbi
-wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz.md5 > ${DIR_HG19}/clinvar.vcf.gz.md5
+wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz -O ${DIR_HG19}/clinvar.vcf.gz
+wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz.tbi -O ${DIR_HG19}/clinvar.vcf.gz.tbi
+wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz.md5 -O ${DIR_HG19}/clinvar.vcf.gz.md5
+md5=`awk '{print $1}'  ${DIR_HG19}/clinvar.vcf.gz.md5`
+echo -e "${md5}\t${DIR_HG19}/clinvar.vcf.gz" > ${DIR_HG19}/clinvar.vcf.gz.md5
 md5sum -c ${DIR_HG19}/clinvar.vcf.gz.md5
 
 # hg38
 mv ${DIR_HG38}/clinvar.vcf.gz ${DIR_HG38}/clinvar_old/clinvar.${DATE}.vcf.gz
 rm ${DIR_HG38}/clinvar.vcf.gz.tbi ${DIR_HG38}/clinvar.vcf.gz.md5
 
-wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz > ${DIR_HG38}/clinvar.vcf.gz
-wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz.tbi > ${DIR_HG38}/clinvar.vcf.gz.tbi
-wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz.md5 > ${DIR_HG38}/clinvar.vcf.gz.md5
+wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz -O ${DIR_HG38}/clinvar.vcf.gz
+wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz.tbi -O ${DIR_HG38}/clinvar.vcf.gz.tbi
+wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz.md5 -O ${DIR_HG38}/clinvar.vcf.gz.md5
+md5=`awk '{print $1}'  ${DIR_HG38}/clinvar.vcf.gz.md5`
+echo -e "${md5}\t${DIR_HG38}/clinvar.vcf.gz" > ${DIR_HG38}/clinvar.vcf.gz.md5
 md5sum -c ${DIR_HG38}/clinvar.vcf.gz.md5
 
