d8d408025655f76dec59836bd3726296e6b20732
Madeline Couse <mhcouse@gmail.com>
2026-06-02 10:42:46 -0400

    also update OMIM for CPHI-DRAGEN-anno

--- utils/update_OMIM_orphanet.sh
+++ utils/update_OMIM_orphanet.sh
@@ -7,7 +7,7 @@
 # 3. After running, you'll need to create and merge PRs in each repo
 #
 # Usage:
-#   sh update_annotations.sh <OMIM_key>
+#   sh update_OMIM_orphanet.sh <OMIM_key>
 #
 # Arguments:
 #   OMIM_key    Access key from OMIM (e.g. lWLUD8lnRx-LhhI9832mXY)
@@ -25,7 +25,7 @@ export PATH=:~/tools/:~/crg2/:~/bioscripts/:~/bioscripts/scripts/:/hpf/largeproj
 # clone fresh instance of cre repo
 git clone https://github.com/ccmbioinfo/cre
 cd cre
-git checkout -b anno-update-${DATE}
+git checkout -b anno-update-${DATE}-hg19
 
 # download OMIM files
 curl -o data/mim2gene.txt https://omim.org/static/omim/data/mim2gene.txt 
@@ -47,7 +47,7 @@ mv orphanet.txt data/orphanet.txt
 # now add all changes to the cre branch and commit
 git add data/hgnc_${DATE}.txt data/genemap2.txt data/mim2gene.txt data/mimTitles.txt data/morbidmap.txt data/OMIM_hgnc_join_omim_phenos_${DATE}.tsv data/OMIM_remaining_unmapped_omim_with_info_${DATE}.tsv data/orphanet.txt cre.vcf2db.R
 git commit -m "Update OMIM and orphanet"
-#git push origin anno-update-${DATE}
+git push origin anno-update-${DATE}-hg19
 
 # update same files for cre hg38 branch
 cd ../
@@ -55,7 +55,7 @@ mv cre cre-hg19
 git clone https://github.com/ccmbioinfo/cre
 cd cre
 git checkout hg38
-git checkout -b anno-update-${DATE} hg38
+git checkout -b anno-update-${DATE}-hg38 hg38
 
 # copy OMIM files
 cp ../cre-hg19/data/OMIM_hgnc_join_omim_phenos_${DATE}.tsv ../cre-hg19/data/OMIM_remaining_unmapped_omim_with_info_${DATE}.tsv data
@@ -69,7 +69,7 @@ sed -i "s/${previous_date}/${DATE}/g" cre.vcf2db.R
 # now add all changes to the cre branch and commit
 git add data/hgnc_${DATE}.txt data/genemap2.txt data/mim2gene.txt data/mimTitles.txt data/OMIM_hgnc_join_omim_phenos_${DATE}.tsv data/OMIM_remaining_unmapped_omim_with_info_${DATE}.tsv data/orphanet.txt cre.vcf2db.R
 git commit -m "Update OMIM and orphanet"
-git push origin anno-update-${DATE}
+git push origin anno-update-${DATE}-hg38
 
 
 # update same files for crg2-pacbio cre
@@ -83,11 +83,30 @@ cp ../cre-hg19/data/genemap2.txt ../cre-hg19/data/mim2gene.txt ../cre-hg19/data/
 cp ../cre-hg19/data/orphanet.txt scripts/cre/data 
 
 # update cre.vcf2db.R
-previous_date=`grep 202 scripts/cre/cre.vcf2db.R  | rev | cut -d '_' -f1 | rev | tr -d '.tsv")'`
+previous_date=`grep 202 scripts/cre/cre.vcf2db.R | grep -v doi | rev | cut -d '_' -f1 | rev | tr -d '.tsv")'`
 sed -i "s/${previous_date}/${DATE}/g" scripts/cre/cre.vcf2db.R
 
 # push changes to crg2-pacbio repo
 git add scripts/cre/data/OMIM_hgnc_join_omim_phenos_${DATE}.tsv scripts/cre/data/orphanet.txt scripts/cre/cre.vcf2db.R scripts/cre/data/genemap2.txt scripts/cre/data/mim2gene.txt scripts/cre/data/mimTitles.txt scripts/cre/data/morbidmap.txt
+git commit -m "Update OMIM and orphanet"
+git push origin anno-update-${DATE}
+
+# update same files for CPHI-DRAGEN-anno
+cd ../
+git clone https://github.com/ccmbioinfo/CPHI-DRAGEN-anno
+cd CPHI-DRAGEN-anno
+git checkout -b anno-update-${DATE}
+
+cp ../cre-hg19/data/OMIM_hgnc_join_omim_phenos_${DATE}.tsv  workflow/scripts/cre/data
+cp ../cre-hg19/data/genemap2.txt ../cre-hg19/data/mim2gene.txt ../cre-hg19/data/mimTitles.txt ../cre-hg19/data/morbidmap.txt  workflow/scripts/cre/data
+cp ../cre-hg19/data/orphanet.txt workflow/scripts/cre/data
+
+# update cre.vcf2db.R
+previous_date=`grep 202 workflow/scripts/cre/cre.vcf2db.R  | grep -v doi | rev | cut -d '_' -f1 | rev | tr -d '.tsv")'`
+sed -i "s/${previous_date}/${DATE}/g" workflow/scripts/cre/cre.vcf2db.R
+
+# push changes to crg2-pacbio repo
+git add workflow/scripts/cre/data/OMIM_hgnc_join_omim_phenos_${DATE}.tsv workflow/scripts/cre/data/orphanet.txt workflow/scripts/cre/cre.vcf2db.R workflow/scripts/cre/data/genemap2.txt workflow/scripts/cre/data/mim2gene.txt workflow/scripts/cre/data/mimTitles.txt workflow/scripts/cre/data/morbidmap.txt
 git commit -m "Update OMIM and orphanet"
 git push origin anno-update-${DATE}
 
